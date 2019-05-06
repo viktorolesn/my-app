de71726de7b34de76ed1c1fc162c4b16665f9066
Viktor <viktormacbook@Viktors-MacBook-Pro.local>
2019-05-06 19:12:36 +0300

    initial commit

--- src/App.js
+++ src/App.js
@@ -2,25 +2,41 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+class App extends React.Component { 
+ //getName(surname){
+   // return "Alexey " + surname;
+  //}
+
+  constructor(){
+    super();
+    this.name = "Alex";
+  }
+  
+  render(){
+  var myStyle = {
+    color:'black',
+    fontSize:16
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header className="App-header">  
+        <input type="button"></input>
+        <input type="button"></input>
+        <input type="button"></input>
+        <input type="button"></input>
       </header>
+      <div className="App-div">
+        <img src={logo} className="App-logo" alt="logo" />  
+        <h1 style={myStyle}> Hello {this.name} </h1> 
+      </div>
     </div>
   );
+  }
 }
 
-export default App;
+class Hello extends React.Component{
+  render(){
+    return <h1>Hello, World</h1>;
+    }
+}
+export default Hello;
